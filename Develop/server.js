33358351953bf746c574cf848ef9a203e71bb076
Zhane Zabala <zhanezabala97@gmail.com>
2021-03-15 17:47:37 -0700

    readme is done... added screenshots of the summary

--- Develop/server.js
+++ Develop/server.js
@@ -18,6 +18,8 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
